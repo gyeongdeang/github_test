8326ba25d729a71ff9ffec5cd27c7d23debd90bd
gyeongdeang <joohyeon0286@gmail.com>
2022-08-22 15:26:35 +0900

    initial2

--- src/main/java/com/example/github_test/controller.java
+++ src/main/java/com/example/github_test/controller.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
 @GetMapping("/")
     public String helloworld(){
-    return "To do Application !";
+    return "To do Application  !";
 }
 }
